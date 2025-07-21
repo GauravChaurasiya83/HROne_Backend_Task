ebae4d4634af90e41d752db792cbe5644963a6e8
GauravChaurasiya83 <gauravchaurasiya870@gmail.com>
2025-07-21 19:49:44 +0530

    edited main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 
 app.include_router(products.router, prefix="/products", tags=["Products"])
 app.include_router(orders.router, prefix="/orders", tags=["Orders"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    
